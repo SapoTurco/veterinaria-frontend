b75b00020aba4a23a666b01a869bc59f872e601d
SapoTurco <garridofranciscojose01@gmail.com>
2026-08-11 23:12:24 -0500

    feat: agregar candado y toggle ver/ocultar en confirmar contraseña del registro

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -277,13 +277,29 @@ async function handleRegister() {
 
           <div class="form-control">
             <label class="label"><span class="label-text font-semibold text-xs text-[#CBCBCB]">Confirmar contraseña *</span></label>
-            <input
-              v-model="form.confirmPassword"
-              type="password"
-              placeholder="Repetir contraseña"
-              class="input input-bordered input-sm w-full input-animated"
-              required
-            />
+            <div class="relative">
+              <input
+                v-model="form.confirmPassword"
+                :type="showPassword ? 'text' : 'password'"
+                placeholder="Repetir contraseña"
+                class="input input-bordered input-sm w-full pl-11 pr-11 input-animated"
+                required
+              />
+              <div class="absolute left-3 top-1/2 -translate-y-1/2 text-[#9a9a9a]">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+              </div>
+              <button type="button" class="absolute right-3 top-1/2 -translate-y-1/2 text-[#9a9a9a] hover:text-[#1A8A8E] transition-colors" @click="showPassword = !showPassword">
+                <svg v-if="!showPassword" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+                </svg>
+              </button>
+            </div>
             <label v-if="form.confirmPassword && form.password !== form.confirmPassword" class="label">
               <span class="label-text-alt text-red-400 text-xs">Las contraseñas no coinciden</span>
             </label>
