e38a2b188b211f5d06a74d798030bf3ba11edde8
SapoTurco <garridofranciscojose01@gmail.com>
2026-08-12 00:22:48 -0500

    fix: boton Agendar cita mismo tamaño que Nueva Mascota

--- src/views/citas/MisCitasView.vue
+++ src/views/citas/MisCitasView.vue
@@ -100,7 +100,7 @@ onMounted(async () => {
           <option value="ATENDIDA">Atendidas</option>
           <option value="CANCELADA">Canceladas</option>
         </select>
-        <button class="btn btn-primary btn-sm h-10 gap-2" @click="router.push('/mis-citas/nueva')">
+        <button class="btn btn-primary gap-2" @click="router.push('/mis-citas/nueva')">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
           </svg>
